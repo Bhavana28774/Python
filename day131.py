6ef9d6d81672f66a3b9a3962b96c1e70562696db
Bhavana28774 <bhavana200205@gmail.com>
2025-02-06 19:25:39 +0530

    Add files via upload

---
+++ day131.py
@@ -0,0 +1,39 @@
+#camera
+import cv2
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Could not access the camera.")
+    exit()
+ret, frame = cap.read()
+if ret:
+    cv2.imwrite("captured_image.jpg", frame)
+    print("Image saved as captured_image.jpg")
+else:
+    print("Error: Could not capture image.")
+cap.release()
+cv2.destroyAllWindows()
+
+#screenshot
+import pyautogui
+
+screenshot = pyautogui.screenshot()
+
+screenshot.save("screenshot.png")
+
+print("Screenshot saved as screenshot.png")
+
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say something...")
+    audio = recognizer.listen(source)
+
+try:
+    text = recognizer.recognize_google(audio)
+    print("You said: " + text)
+except sr.UnknownValueError:
+    print("Sorry, I could not understand the audio")
+except sr.RequestError as e:
+    print(f"Could not request results from Google Speech Recognition service; {e}")
